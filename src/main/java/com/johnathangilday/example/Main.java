4ce839bca267a46ff9a521f219049b1269d58f22
Johnathan Gilday <me@johnathangilday.com>
2019-04-16 21:44:48 -0400

    :tada: Basic Maven project
    Generates a jar, source jar, and javadoc jar, all of which are required by Maven Central.
    
    Use `maven-flatten-plugin` to flatten the pom keeping only the information required by Maven Central

---
+++ src/main/java/com/johnathangilday/example/Main.java
@@ -0,0 +1,12 @@
+package com.johnathangilday.example;
+
+/** Simple hello world program to demonstrate deploying a jar to Maven Central */
+public final class Main {
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, world!");
+  }
+
+  /** static members only */
+  private Main() {}
+}
